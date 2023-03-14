7ad0ce598e88cc459ad24782d8a9f6553b819525
Workineh <Workinehw94@gmail.com>
2023-03-14 10:57:29 +0000

    commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+"""
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+"""
 
 
-def make_change(coins, total):
-    """Determines the fewest number of coins needed \
-        to meet a given amount total"""
+def makeChange(coins, total):
+    """
+    Determines the fewest coins to meet total
+
+    Args:
+        coins (list): list of coins
+        total (int): value to meet as total
+
+    Returns:
+        coins (int): fewest number of coins needed to meet total
+        (0) if total is 0 or less
+        (-1) if no number of the coins can meet the total
+    """
     if total <= 0:
         return 0
-
-    current_total = 0
-    used_coins = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        r = (total - current_total) // coin
-        current_total += r * coin
-        used_coins += r
-        if current_total == total:
-            return used_coins
-    return -1
+    min_coins = [0] + [float('inf')] * total
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                num_coins = min_coins[i - coin] + 1
+                if num_coins < min_coins[i]:
+                    min_coins[i] = num_coins
+    if min_coins[-1] == float("inf"):
+        return -1
+    return min_coins[total]
